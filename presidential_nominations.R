bb6f5012261dadba0adf8674f19519879bf428c5
Cody Drolc <30817107+codydrolc@users.noreply.github.com>
2018-10-18 13:04:54 -0500

    converted to function and simplified code

--- presidential_nominations.R
+++ presidential_nominations.R
@@ -1,53 +1,66 @@
 #-------------------------------------------------------------------------------
-#---- Scrape Congress.gov nominations data
-# Created: 6/21/2018
-# Modified: 6/22/2018
+#---- Scrape GAO Congressional Review Act data
+# Created: 1/17/2018
+# Modified: 10/18/2018
 
-# Note: This function is still VERY much in development!
+# Note: GAO stores the bolded rule information in a single table on each page.
+# There is only one table on each page. So, parsing the html nodes only requires
+# taking "table" and nothing else. Converting the "table" from a list to a data
+# frame creates "long" format data that is reshaped by the function. As a result,
+# each time the function is pulled there will only be one row. 
 
-# Major issue: Not all nominee names are contained in a single table. The current
-# function does not handle/parse multiple tables well.
+# An example loop is provided that takes the universe of rules submitted
+# under the Congressional Review Act.
+
+#-------------------------------------------------------------------------------
 
 # Load packages
-library(rvest); library(tidyverse)
+library(tidyverse); library(plyr)
 
-# Scraping function
-# Congress.gov data run from 1981 (the 97th Congress) to the present
-pullnomz <- function(cong, pn) {
-  try(   # Should keep running if page does not exist
-    page <- read_html(paste('https://www.congress.gov/nomination/', cong, '-congress/', pn, sep = ''))
-  )
-  title <- html_node(page, 'h1') %>% # Title of page
-    html_text()
-  description <- html_nodes(page, 'ul.plain.little_margin') %>% # Basic info
-    html_text() %>% 
-    str_replace_all("[\r\n]" , "")
-  if(length(description) == 3) { # Some pages have limited information; committee and final/latest action tend to be missing
-    nominee_description <- description[[1]]
-    organization <- description[[2]]
-    rcvd_president <- description[[3]]
-  } else{ 
-  nominee_description <- description[[1]]
-  organization <- description[[2]]
-  latest_action <- description[[3]]
-  rcvd_president <- description[[4]]
-  committee <- description[[5]]
-  }
-  nominee_names <- as.data.frame(html_table(page)) # Names of all nominees
-  if(colnames(nominee_names)[[1]] != "Nominee") { return(NA)
+# Scraping function specific to the Government Accountability Office
+pull_cra <- function(cn) {
+  if(nchar(as.integer(cn)) != 6) {
+    stop("Control number must be 6 digits")
   } else {
-    return(nominee_data <- data.frame(title = title,
-                                      nominee_description = nominee_description,
-                                      organization = organization,
-                                      latest_action = latest_action,
-                                      rcvd_president = rcvd_president,
-                                      committee = committee, 
-                                      nominee_names = nominee_names[, 1]))
-  } # close if else
-} # close function
+    page <- read_html(paste0("https://www.gao.gov/fedrules/", cn)) # Fill control number
+    table <- html_table(page, fill = TRUE) # Find only number on page
+    if(length(table) == 0) { # No data if control number is not used
+      warning("No data found")
+      return(data.frame(control_number = cn,
+                        no_data = 1))
+    } else {
+      table <- table[[1]] %>% # Table to data frame
+        spread(X1, X2)
+      names <- colnames(table) %>% # Create acceptable names
+        str_remove_all(":|\\.|-") %>% 
+        str_replace_all(" ", "_") %>%
+        tolower()
+      names(table) <- names
+      return(table)
+    }
+  }
+}
 
-# Produce NA
-test <- pullnomz(98, 1)
+pull_cra(123) # Should stop
+pull_cra(123456) # No warnings
+pull_cra(200000) # Warning that there is no data
 
-# 111 nominations
-test <- pullnomz(97, 1)
+# Null object to fill
+cra <- NULL
+
+# Loop over control numbers
+# The range pulls the "universe" of federal rules (~76,000) submitted to GAO
+for (i in 100000:200000) {
+  print(i)
+  bind <- pull_cra(i)
+  cra <- rbind.fill(cra, bind) # Combine iterations
+}
+
+# Removing rows where the control number returned no data
+cra <- cra %>% filter(no_data != 1) %>% select(-no_data)
+
+# Convert dates
+cra$received <- mdy(cra$received)
+cra$effective <- mdy(cra$effective) # Might be missing
+cra$published <- mdy(cra$published) # Might be missing
+cra <- arrange(cra$received)
