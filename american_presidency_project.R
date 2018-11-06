608ab375cc5137f4db0cbb72d40f9fe48694bf82
Cody Drolc <30817107+codydrolc@users.noreply.github.com>
2018-11-06 11:28:46 -0600

    Updated to work with the APP website overhaul

--- american_presidency_project.R
+++ american_presidency_project.R
@@ -5,57 +5,39 @@
 # statements. It relies on UCSB's The American Presidency Project (APP) to 
 # create a corpus.
 
-# Packages
-library(tidyverse) # Cleaning data and dates
-library(tidytext)  # Basic sentiment analysis
-
 # Build function that accepts document identifier
-  
+
 pullprez <- function(id) {
-  url <- paste('http://www.presidency.ucsb.edu/ws/index.php?pid=', id, sep = '')
+  # Packages
+  require(rvest)     # For scraping
+  require(lubridate) # For dates
+  require(tidyverse) # Cleaning data
+  require(tidytext)  # For basic word count
+  url <- paste('https://www.presidency.ucsb.edu/node/', id, sep = '')
   encoding <- guess_encoding(url)
   try( # Don't crash if there is an error
     data <- read_html(url, encoding = encoding$encoding[1]) # Encoding with highest probability
   )
   title <- data %>%        # Page title
-    html_node('title') %>%
+    html_node('div.field-ds-doc-title') %>%
     html_text()
   date <- data %>%         # Page date
-    html_node('.docdate') %>%
+    html_node('span.date-display-single') %>%
     html_text() %>%
     mdy() %>%
     as.character()
-  collection <- data %>%   # American presidency collection
-    html_nodes('td.ver10') %>%
-    html_node('img') %>%
-    html_attr('alt') %>%
-    paste(collapse = ' ')
-    collection <- sub("NA.*", "", collection) # Clean up string
-    collection <- sub("<.*", "", collection)  # Further cleaning
-  text <- data %>%        # Page text
-    html_nodes('span.displaytext') %>%
+  president <- data %>%   # President
+    html_node('div.field-title') %>%
     html_text() %>%
     paste(collapse = ' ')
-  president <- gsub("\\:.*", "", title) # President, VP, FLOTUS, etc.
-  title <- str_sub(title, (str_length(president) + 2)) # Remove president's name from title
-  #--- Sentiment from document
+  text <- data %>%        # Page text
+    html_nodes('div.field-docs-content') %>%
+    html_text() %>%
+    paste(collapse = ' ')
+  #--- Word count from document
   tokens <- data_frame(text = text) %>% unnest_tokens(word, text)
-  sentiment <- tokens %>%
-    inner_join(get_sentiments("bing")) %>%
-    count(sentiment)  # Count negative and postive words
-  sentiment <-        # Data to wide, if error, return fake object with 0
-    try(
-      sentiment %>%
-        spread(sentiment, n, fill = 0))
-  if (length(sentiment) == 0) {
-    # Create fake object with 0 for positive and negative
-    sentiment <- NULL
-    sentiment$positive <- 0
-    sentiment$negative <- 0
-    sentiment <- as.data.frame(sentiment)
-  }
-  return(as.data.frame(cbind(id, president, date, collection, title, text, sentiment, words = nrow(tokens))))
+  return(as.data.frame(cbind(id, date, president, title, text, words = nrow(tokens))))
 }
 
 # Test function
-function.test <- pullprez(20832)
+function.test <- pullprez(266619)
